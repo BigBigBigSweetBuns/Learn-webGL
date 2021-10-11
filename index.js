b2da62ae4b8406d6d2f86bcd86628a0c58ca6720
doushabao <dadadadoushabao@gmail.com>
2021-10-11 17:15:20 +0800

    生成一个三角形
    相关链接：https://webglfundamentals.org/webgl/lessons/zh_cn/webgl-fundamentals.html

--- index.js
+++ index.js
@@ -2,25 +2,33 @@ window.onload = function () {
     webGL();
 }
 
-function resize(canvas) {
-    let displayWidth = canvas.clientWidth;
-    let displayHeight = canvas.clientHeight;
-    if (canvas.width != displayWidth || canvas.height != displayHeight) {
-        canvas.width = displayWidth;
-        canvas.height = displayHeight;
+function createShader(gl, type, source) {
+    var shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+    var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+    if (success) {
+        return shader;
     }
-}
-// 渲染实际像素
-function resizePixels(canvas) {
-    const realToCSSPixels = window.devicePixelRatio;
 
-    let displayWidth = Math.floor(canvas.clientWidth * realToCSSPixels);
-    let displayHeight = Math.floor(canvas.clientHeight * realToCSSPixels);
-    if (canvas.width != displayWidth || canvas.height != displayHeight) {
-        canvas.width = displayWidth;
-        canvas.height = displayHeight;
-    }
+    console.log(gl.getShaderInfoLog(shader));
+    gl.deleteShader(shader);
 }
+
+function createProgram(gl, vertexShader, fragmentShader) {
+    var program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+    var success = gl.getProgramParameter(program, gl.LINK_STATUS);
+    if (success) {
+        return program;
+    }
+
+    console.log(gl.getProgramInfoLog(program));
+    gl.deleteProgram(program);
+}
+
 function webGL() {
     const canvas = document.querySelector("#glcanvas");
     // 初始化WebGL上下文
@@ -32,12 +40,88 @@ function webGL() {
         return;
     }
 
-    resize(canvas);
-    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+    // todo 
+    // 推测：初始化buffer,绑定新的空buffer
+    // 正解：将绑定点绑定到缓冲数据（positionBuffer）
+    let positionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
-    // 使用完全不透明的黑色清除所有图像
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    // 用上面指定的颜色清除缓冲区
-    gl.clear(gl.COLOR_BUFFER_BIT);
+    // Get the strings for our GLSL shaders
+    var vertexShaderSource = document.querySelector("#vertex-shader-2d").text;
+    var fragmentShaderSource = document.querySelector("#fragment-shader-2d").text;
 
+    // create GLSL shaders, upload the GLSL source, compile the shaders
+    var vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+    var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+
+    // Link the two shaders into a program
+    var program = createProgram(gl, vertexShader, fragmentShader);
+
+    // look up where the vertex data needs to go.
+    var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+
+
+    setGeometry(gl);
+    drawScene();
+
+    function resize(canvas) {
+        let displayWidth = canvas.clientWidth;
+        let displayHeight = canvas.clientHeight;
+        if (canvas.width != displayWidth || canvas.height != displayHeight) {
+            canvas.width = displayWidth;
+            canvas.height = displayHeight;
+        }
+    }
+    // 渲染实际像素大小画面
+    function resizePixels(canvas) {
+        const realToCSSPixels = window.devicePixelRatio;
+
+        let displayWidth = Math.floor(canvas.clientWidth * realToCSSPixels);
+        let displayHeight = Math.floor(canvas.clientHeight * realToCSSPixels);
+        if (canvas.width != displayWidth || canvas.height != displayHeight) {
+            canvas.width = displayWidth;
+            canvas.height = displayHeight;
+        }
+    }
+
+    // 添加三角形
+    function setGeometry(gl) {
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([0, 0,
+            0, 0.5,
+            0.2, 0,]),
+            gl.STATIC_DRAW);
+    }
+
+    function drawScene() {
+
+        resize(canvas);
+        gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+        // 使用完全不透明的黑色清除所有图像
+        gl.clearColor(0.0, 0.0, 0.0, 1.0);
+        // 用上面指定的颜色清除缓冲区
+        gl.clear(gl.COLOR_BUFFER_BIT);
+
+
+        // Tell it to use our program (pair of shaders)
+        gl.useProgram(program);
+        // Turn on the attribute
+        gl.enableVertexAttribArray(positionAttributeLocation);
+        // 将绑定点绑定到缓冲数据（positionBuffer）
+        gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+        // 告诉属性怎么从positionBuffer中读取数据 (ARRAY_BUFFER)
+        let size = 2;          // 每次迭代运行提取两个单位数据
+        let type = gl.FLOAT;   // 每个单位的数据类型是32位浮点型
+        let normalize = false; // 不需要归一化数据
+        let stride = 0;        // 0 = 移动单位数量 * 每个单位占用内存（sizeof(type)）
+        // 每次迭代运行运动多少内存到下一个数据开始点
+        let offset = 0;        // 从缓冲起始位置开始读取
+        gl.vertexAttribPointer(
+            positionAttributeLocation, size, type, normalize, stride, offset);
+
+        let primitiveType = gl.TRIANGLES;
+        offset = 0;
+        let count = 3;
+        gl.drawArrays(primitiveType, offset, count);
+    }
 }
